735af042543c356b9a2abfaf1a881cbbf9dec182
h3568301 <anson940@connect.hku.hk>
2025-09-17 11:56:36 +0100

    first commit

---
+++ chatBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.Services.AddChatClient(chatClientBuilder =>
+    chatClientBuilder.UseOpenAI(apiKey: "your-api-key-here"));
+
+var host = builder.Build();
+
+var chatClient = host.Services.GetRequiredService<IChatClient>();
+
+var chatCompletion = await chatClient.CompleteAsync("What is .NET");
+
+Console.WriteLine(chatCompletion.Message.Text);
+
+await host.RunAsync();
